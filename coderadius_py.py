f0676f2e666325f17eaee10285dc48ef28601320
sakshighag <87032028+sakshighag@users.noreply.github.com>
2021-07-11 13:07:07 +0530

    Add files via upload

---
+++ coderadius_py.py
@@ -0,0 +1,11 @@
+# -pi= 3.14
+r = float(input ("Input the radius of the circle :"))
+print ("The area of the circle with radius" = pi*r*r
+
+       *- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
